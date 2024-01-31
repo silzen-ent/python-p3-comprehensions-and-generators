80398e1589d751cb93f9e997090ccc3c759ddfc5
Alan Silva <silzen.ent@gmail.com>
2024-01-31 12:49:51 -0700

    Completed List Comprehensions & Gen Exp Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
+# Using a list comprehension, write a function return_evens() that returns a list of all of the even elements of a sequence of integers.
 def return_evens(num_list):
-    pass
+    return [num for num in num_list if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
+
+
+def make_exclamation(sentence_list):    
+    return [sentence + '!' for sentence in sentence_list]
